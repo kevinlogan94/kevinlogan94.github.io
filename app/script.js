c2e5d6aabe0d4d3bf4f9f51b03767902357decab
Kevin Logan <kevlogan7@gmail.com>
2017-10-08 22:17:09 -0400

    Added project section and menu icon for mobile

--- app/script.js
+++ app/script.js
@@ -15,4 +15,15 @@ $(document).ready(function () {
             scrollTop: 0
         }, 1000);
     });
+    $('.Projects').click(function () {
+        $('html,body').animate({
+            scrollTop: $('#Projects').offset().top - 35
+        }, 1000);
+    });
+
+    /* ------- */
+    
+    $(".container").click(function(){
+        $(".container").toggleClass('changed');
+      });
 })
