60618c51cd26eb3df44908fc67ccb08381fe91b1
Kevin Logan <kevlogan7@gmail.com>
2017-10-22 18:47:55 -0400

    Remove top nav in mobile

--- app/script.js
+++ app/script.js
@@ -1,29 +1,36 @@
 $(document).ready(function () {
     
+    // Work scroll animation
     $('.Work').click(function () {
-        $('html,body').animate({
-            scrollTop: $('#work').offset().top - 35
-        }, 1000);
+        desktopWidth();
+        if(em >= 48) { $('html,body').animate({scrollTop: $('#work').offset().top - 35}, 1000); }
+        else { $('html,body').animate({scrollTop: $('#work').offset().top + 5}, 1000); }
     });
-    $('.Doing').click(function () {
-        $('html,body').animate({
-            scrollTop: $('#Doing').offset().top - 35
-        }, 1000);
+
+    // The Now scroll animation
+    $('.TheNow').click(function () {
+        desktopWidth();
+        if(em >= 48) { $('html,body').animate({scrollTop: $('#TheNow').offset().top - 35}, 1000); }
+        else { $('html,body').animate({scrollTop: $('#TheNow').offset().top + 5}, 1000); }
     });
+
+    // Home scroll animation
     $('.Home').click(function () {
-        $('html,body').animate({
-            scrollTop: 0
-        }, 1000);
+        $('html,body').animate({ scrollTop: 0 }, 1000);
     });
+
+    // Projects scroll animation
     $('.Projects').click(function () {
-        $('html,body').animate({
-            scrollTop: $('#Projects').offset().top - 35
-        }, 1000);
+        desktopWidth();
+        if(em >= 48) { $('html,body').animate({scrollTop: $('#Projects').offset().top - 35}, 1000); }
+        else { $('html,body').animate({scrollTop: $('#Projects').offset().top + 5}, 1000); }
     });
-    $('.Contact').click(function () {
-        $('html,body').animate({
-            scrollTop: $('#Contact').offset().top - 35
-        }, 1000);
+
+    // Connect scroll animation
+    $('.Connect').click(function () {
+        desktopWidth();
+        if(em >= 48) { $('html,body').animate({scrollTop: $('#Connect').offset().top - 35}, 1000); }
+        else { $('html,body').animate({scrollTop: $('#Connect').offset().top + 5}, 1000); }
     });
 
     /* --- Click anything and the menu disapears and icon gets unflipped. ---- */
@@ -38,8 +45,11 @@ $(document).ready(function () {
     /* ---- Click menu icon--- */
     
     $('.container').click(function(evt){
-        evt.stopPropagation();
+        evt.stopPropagation(); //stops 'body' event handler from removing these classes.
         $(this).toggleClass('changed');
         $('.mobile-menu-item').toggleClass('mobile-menu-active');
     });
 })
+
+var em;
+function desktopWidth() { em = $(window).width() / parseFloat($("html").css("font-size")); }
