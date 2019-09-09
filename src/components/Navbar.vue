5b44b8e9019b85a2e9a12b25fcbcb216f80755b2
Kevin Logan <kevlogan7@gmail.com>
2019-09-09 08:45:40 -0400

    setup navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,42 @@
 <template>
-  <nav></nav
-></template>
+  <nav class="navbar has-shadow is-spaced ">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://github.com/kevinlogan94">
+          <img src="../assets/logo.png" alt="vue-a11y repository" />
+        </a>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar"
+          v-on:click="isHamburgerMenuActive = !isHamburgerMenuActive"
+          v-bind:class="{ 'is-active': isHamburgerMenuActive }"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div
+        id="navbar"
+        class="navbar-menu"
+        v-bind:class="{ 'is-active': isHamburgerMenuActive }"
+      >
+        <div
+          class="navbar-start"
+          v-on:click="isHamburgerMenuActive = !isHamburgerMenuActive"
+        >
+          <a role="button" class="navbar-item">Kevin</a>
+          <a role="button" class="navbar-item">What I do</a>
+          <a role="button" class="navbar-item">My Work</a>
+          <a role="button" class="navbar-item">Contact Me</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
 
 <script>
 export default {
