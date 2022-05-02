845de432f6bb7f590126f8627b17058dcee5f1c5
Kevin Logan <klogan@navistone.com>
2022-05-02 17:11:03 -0400

    :bug: fix navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,31 +23,49 @@
       <a class="navbar-item" href="https://github.com/kevinlogan94"
         ><o-icon variant="white" icon="github" title="Github Icon"></o-icon
       ></a>
-    </div>
-    <div class="navbar-end">
-      <a class="navbar-item has-text-white" @click="scrollIntoView('aboutMe')"
-        >About Me</a
-      >
-      <a class="navbar-item has-text-white" @click="scrollIntoView('passions')"
-        >Passions</a
-      >
-      <a class="navbar-item has-text-white" @click="scrollIntoView('software')"
-        >Software</a
-      >
       <a
-        class="navbar-item has-text-white"
-        @click="scrollIntoView('meditation')"
-        >Meditation</a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        @click="menuToggle"
+        :class="{ 'is-active': menuActive }"
       >
-      <a class="navbar-item has-text-white" @click="scrollIntoView('games')"
-        >Games</a
-      >
-      <a class="navbar-item has-text-white" @click="scrollIntoView('library')"
-        >Library</a
-      >
-      <a class="navbar-item has-text-white" @click="scrollIntoView('connect')"
-        >Connect</a
-      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" :class="{ 'is-active': menuActive }">
+      <div class="navbar-end">
+        <a class="navbar-item has-text-white" @click="scrollIntoView('aboutMe')"
+          >About Me</a
+        >
+        <a
+          class="navbar-item has-text-white"
+          @click="scrollIntoView('passions')"
+          >Passions</a
+        >
+        <a
+          class="navbar-item has-text-white"
+          @click="scrollIntoView('software')"
+          >Software</a
+        >
+        <a
+          class="navbar-item has-text-white"
+          @click="scrollIntoView('meditation')"
+          >Meditation</a
+        >
+        <a class="navbar-item has-text-white" @click="scrollIntoView('games')"
+          >Games</a
+        >
+        <a class="navbar-item has-text-white" @click="scrollIntoView('library')"
+          >Library</a
+        >
+        <a class="navbar-item has-text-white" @click="scrollIntoView('connect')"
+          >Connect</a
+        >
+      </div>
     </div>
   </nav>
 </template>
@@ -57,11 +75,17 @@ import { Options, Vue } from "vue-property-decorator";
 
 @Options({})
 export default class NavBar extends Vue {
+  menuActive = false;
+
   scrollIntoView(classNameOfSection: string) {
     const element = document.getElementsByClassName(classNameOfSection)[0];
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
     }
+  }
+
+  menuToggle() {
+    this.menuActive = !this.menuActive;
   }
 }
 </script>
